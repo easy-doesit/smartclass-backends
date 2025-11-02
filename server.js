b64a4232e512c3711006706d368701cfa2073eae
easy-doesit <info.cbtportal@gmail.com>
2025-11-02 18:11:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,10 +21,8 @@ app.post("/api/chat", async (req, res) => {
 
     try {
       const response = await fetch(
-       const GEMINI_URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" +
-  process.env.GEMINI_API_KEY;
-
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" +
+          process.env.GEMINI_API_KEY,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
